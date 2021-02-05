c8d627511405d1fe46e02a9add747dc31deefdb3
joshsrai <joshsrai@gmail.com>
2021-02-05 08:30:03 -0500

    commit tree 4f0b0bd25d9b2fad5acada41726f114fd007c51c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+const map = function(arr, cb) {
+    const newArr = []
+    for (let i = 0; i < arr.length; i++){
+        newArr.push(cb(arr[i]))
+    }
+    return newArr
+}
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
